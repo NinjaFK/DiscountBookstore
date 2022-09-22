4754ba10d0680be98285a7361f2485584186166c
Brian <nieh_brian@yahoo.com>
2022-09-22 11:50:32 -0700

    Added all header functions into memberTypeimp

---
+++ memberTypeImp.cpp
@@ -0,0 +1,84 @@
+
+#include <memberType.h>
+#include <iostream>
+
+void memberType::setMemberInfo(string ID, string fName, string lName,
+                               int bPurchased, double amount, double discount, int membership)
+{
+    memberID = ID;
+    firstName = fName;
+    lastName = lName;
+    booksPurchased = bPurchased;
+    amountSpent = amount;
+    booksDiscount = discount;
+    lengthOfMembership = membership;
+}
+void memberType::setMemberID(string ID)
+{
+    memberID = ID;
+}
+void memberType::setName(string fName, string lName)
+{
+    firstName = fName;
+    lastName = lName;
+}
+
+bool memberType::checkID(string)
+{
+}
+bool memberType::checkBookCount(int booksCount)
+{
+    if (booksCount > 0)
+    {
+        return 1;
+    }
+    return 0;
+}
+bool memberType::checkAmountSpent(int booksCount, double amountSpent)
+{
+}
+bool memberType::checkMembership(int membership)
+{
+}
+bool memberType::checkDiscount(int discount)
+{
+}
+
+string memberType::getID() const
+{
+    return memberID;
+}
+string memberType::getFirstName() const
+{
+    return firstName;
+}
+string memberType::getLastName() const
+{
+    return lastName;
+}
+int memberType::getBookCount() const
+{
+    return booksPurchased;
+}
+double memberType::getPurchaseBill() const
+{
+    return booksDiscount;
+}
+int memberType::getLenghtOfMembership() const
+{
+    return lengthOfMembership;
+}
+double memberType::getDiscount() const
+{
+    return booksDiscount;
+}
+double memberType::getDiscountAmount() const
+{
+}
+double memberType::getFinalPrice() const
+{
+}
+
+memberType::~memberType()
+{
+}
--- test.cpp
+++ test.cpp
@@ -1,13 +1,14 @@
 #include <iostream>
+using namespace std;
 
 int main(int args, char *argv[])
 {
-    std::cout << args << '\n';
+    cout << args << '\n';
     for (int i = 0; i < args; i++)
     {
-        std::cout << argv[i] << '\n';
+        cout << argv[i] << '\n';
     }
-    std::ifstream infile;
+    ifstream infile;
     infile.open(argv[1]);
     return 0;
 }
