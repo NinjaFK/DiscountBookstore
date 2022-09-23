ee4606d9c5df37729e9cb44d9e832fe456048c35
Brian <nieh_brian@yahoo.com>
2022-09-23 12:16:21 -0700

    tested checkID in test.cpp

--- test.cpp
+++ test.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 using namespace std;
 
+/*
 int main(int args, char *argv[])
 {
     cout << args << '\n';
@@ -10,5 +11,35 @@ int main(int args, char *argv[])
     }
     ifstream infile;
     infile.open(argv[1]);
+    return 0;
+}
+*/
+
+bool checkID(string ID)
+{
+    if (ID.size() != 6)
+    {
+        return 0;
+    }
+    for (int i = 0; i < 3; i++)
+    {
+        if (ID[i] < 65 || (ID[i] > 90 && ID[i] < 97) || ID[i] > 122)
+        {
+            return 0;
+        }
+    }
+    for (int i = 3; i < 6; i++)
+    {
+        if (ID[i] < 48 || ID[i] > 57)
+        {
+            return 0;
+        }
+    }
+    return 1;
+}
+
+int main()
+{
+    cout << checkID("apc123") << endl;
     return 0;
 }
