5f53dcc9ae8b708ce1b3893ab8e24b418ca7979b
Brian <nieh_brian@yahoo.com>
2022-09-24 15:58:46 -0700

    Added FileClass constuct and decon

--- fileioImp.cpp
+++ fileioImp.cpp
@@ -3,13 +3,16 @@
 #include <fstream>
 #include <string>
 #include <iomanip>
+#include <sstream>
 
 FileClass::FileClass(string file_name)
 {
+    inFile.open(file_name);
 }
 
 FileClass::~FileClass()
 {
+    inFile.close();
 }
 
 bool getNextField(string &line, int &index, string &subString)
@@ -17,7 +20,12 @@ bool getNextField(string &line, int &index, string &subString)
 }
 unsigned int getMembersDataFromFile(ifstream &inFile, memberType storeMembers[500])
 {
+    string line;
+    string partLine;
+    getline(inFile, line);
+    stringstream ss(line);
 }
 void writeMembersDataToFile(ofstream &outFile, memberType storeMembers[500], unsigned int totalEntry)
 {
+    ofstream outfile("membersData_output.txt");
 }
