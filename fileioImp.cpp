0f5fe7d87cd28584ec7e0cf81a45a9852f376c8c
Brian <nieh_brian@yahoo.com>
2022-09-25 22:32:40 -0700

    Finished writemembersdatatofile and add comments for codegrade

--- fileioImp.cpp
+++ fileioImp.cpp
@@ -1,3 +1,9 @@
+/*
+    Name: Brian, 5008139573, 1005, 2
+    Description: Discount bookstore
+    Input: membersdata.txt
+    Output: membersdata_output.txt
+*/
 #include <iostream>
 #include <fstream>
 #include <string>
@@ -6,20 +12,40 @@
 #include "memberType.h"
 #include "fileio.h"
 using namespace std;
-
 FileClass::FileClass(string file_name)
 {
+    // Opens file with file_name
     inFile.open(file_name);
+    // checks if file is vaild then prints if its not
+    if (!inFile)
+    {
+        cout << "################################"
+             << "###########################"
+             << '\n'
+             << "Error : Input file not present : "
+             << file_name << '\n'
+             << "#################################"
+             << "##########################"
+             << '\n';
+    }
+    // opens the the output file
+    outFile.open("membersData_output.txt");
 }
-
+// deconstucts by closing
+// both files
 FileClass::~FileClass()
 {
+    // close both files
     inFile.close();
     outFile.close();
 }
-
-unsigned int getMembersDataFromFile(ifstream &inFile, memberType storeMembers[500])
+// returns unsigned int of
+// many members that are read
+unsigned int getMembersDataFromFile(ifstream &inFile,
+                                    memberType storeMembers[500])
 {
+    // creates all vars needed to put into
+    //  storeMembers
     string line;
     string memberID = "";
     string firstName = "";
@@ -28,12 +54,20 @@ unsigned int getMembersDataFromFile(ifstream &inFile, memberType storeMembers[50
     double amountSpent = 0.0;
     double booksDiscount = 0.0;
     int lengthOfMembership = 0;
+    // count is what gets returned
     int count = 0;
+    // gets rid of first line
     getline(inFile, line);
     stringstream ss(line);
+    // while loop while the next line is vaild
     while (getline(inFile, line))
     {
+        // creates memberType object into storemembers
         storeMembers[count] = memberType();
+        // sets ss to line and puts
+        // data in to each var
+        // stringstream also typecasts
+        // so doing this is easier
         ss.str(line);
         ss >> memberID;
         ss >> firstName;
@@ -42,14 +76,103 @@ unsigned int getMembersDataFromFile(ifstream &inFile, memberType storeMembers[50
         ss >> amountSpent;
         ss >> lengthOfMembership;
         ss >> booksDiscount;
-        storeMembers[count].setMemberInfo(memberID, firstName, lastName, booksPurchased, amountSpent, booksDiscount, lengthOfMembership);
+        // puts data from var to array of memberType
+        storeMembers[count].setMemberInfo(memberID, firstName,
+                                          lastName, booksPurchased,
+                                          amountSpent, booksDiscount,
+                                          lengthOfMembership);
         count++;
     }
     return count;
 }
-
-void writeMembersDataToFile(ofstream &outFile, memberType storeMembers[500], unsigned int totalEntry)
+// writes to outputfile using amount
+// from getMembersDataFromFile and
+// addeds extra data like amount of books
+// vaild members and money
+void writeMembersDataToFile(ofstream &outFile,
+                            memberType sm[500], unsigned int totalEntry)
 {
-    // ofstream outfile("membersData_output.txt");
-    // totalEntry = 1;
+    // creates var for vaild amount, books, total money
+    int vaild = 0;
+    int books = 0;
+    double total = 0.0;
+    // prints first line of file
+    outFile << "ID"
+            << " firstName"
+            << " lastName"
+            << " bookCount"
+            << " purchaseBill"
+            << " LengthofMemberShip"
+            << " discountPercentage"
+            << " discountedAmount"
+            << " finalPrice" << '\n';
+
+    // for loop that prints all data to outfile
+    for (int i = 0; i < int(totalEntry); i++)
+    {
+        // prints out file
+        // with precision set to 2
+        // also uses fixed to not
+        // use scienctific notation
+        // and adds a space in between
+        outFile << setprecision(2) << fixed
+                << sm[i].getID() << " " << sm[i].getFirstName() << " "
+                << sm[i].getLastName() << " "
+                << sm[i].getBookCount() << " "
+                << sm[i].getPurchaseBill() << " "
+                << sm[i].getLenghtOfMembership() << " "
+                << sm[i].getDiscount() << " ";
+        // checks for invalid id
+        if (!sm[i].checkID(sm[i].getID()))
+        {
+            outFile << "invalid ID" << '\n';
+        }
+        // checks for invalid BookCount
+        else if (!sm[i].checkBookCount(sm[i].getBookCount()))
+        {
+            outFile << "invalid Book purchased count" << '\n';
+        }
+        // checks for invalid AmountSpent
+        // this takes two inputs
+        else if (!sm[i].checkAmountSpent(sm[i].getBookCount(),
+                                         sm[i].getPurchaseBill()))
+        {
+            outFile << "invalid purchaseBill" << '\n';
+        }
+        // checks for invalid Membership
+        else if (!sm[i].checkMembership(sm[i].getLenghtOfMembership()))
+        {
+            outFile << "invalid Length Of Membership" << '\n';
+        }
+        // checks for invalid Discount
+        else if (!sm[i].checkDiscount(sm[i].getDiscount()))
+        {
+            outFile << "invalid discountPercentage" << '\n';
+        }
+        // if passing all checks it does
+        // disamount and final price
+        else
+        {
+            // adding values every time is vaild
+            vaild++;
+            books += sm[i].getBookCount();
+            total += sm[i].getFinalPrice();
+            outFile << sm[i].getDiscountAmount()
+                    << " " << sm[i].getFinalPrice() << '\n';
+        }
+    }
+    // prints extra data
+    // and the dasses and plus
+    outFile << '\n'
+            << '\n'
+            << "------------+------------+------------+"
+            << "------------+------------+------------"
+            << "+------------+------------+" << '\n';
+    outFile << "Total entries from file : " << totalEntry << '\n';
+    outFile << "Valid entries : " << vaild << '\n';
+    outFile << "Total books Sold : " << books << '\n';
+    outFile << "TotalSaleAmount : " << total << '\n';
+    outFile << "------------+------------+------------+"
+            << "------------+------------+------------"
+            << "+------------+------------+" << '\n';
 }
--- memberTypeImp.cpp
+++ memberTypeImp.cpp
@@ -1,8 +1,15 @@
+/*
+    Name: Brian, 5008139573, 1005, 2
+    Description: Discount bookstore
+    Input: membersdata.txt
+    Output: membersdata_output.txt
+*/
 #include "memberType.h"
 #include <iostream>
-
+// class constuctor
 memberType::memberType()
 {
+    // sets default values
     memberID = "sbc123";
     firstName = "fName";
     lastName = "lName";
@@ -11,47 +18,67 @@ memberType::memberType()
     booksDiscount = 0;
     lengthOfMembership = 0;
 }
-
+// sets private vars using passed parameters
+// also does logic for adding discount
+// if booksPurchased is more than 4
 void memberType::setMemberInfo(string ID, string fName, string lName,
-                               int bPurchased, double amount, double discount, int membership)
+                               int bPurchased, double amount,
+                               double discount, int membership)
 {
+    // sets data from parameters
     memberID = ID;
     firstName = fName;
     lastName = lName;
     booksPurchased = bPurchased;
     amountSpent = amount;
     booksDiscount = discount;
+    // checks if books purchased is more
+    // than 4 then adds discount if true
     if (booksPurchased >= 4)
     {
+        // doesnt set because it could be a value
         booksDiscount += 4;
     }
     lengthOfMembership = membership;
 }
+// sets memberid using passed parameter
 void memberType::setMemberID(string ID)
 {
     memberID = ID;
 }
+// sets name using passed parameter
 void memberType::setName(string fName, string lName)
 {
     firstName = fName;
     lastName = lName;
 }
-
+// checks if Id is vaild
+// so is three letters
+// and three letteres on the end
 bool memberType::checkID(string ID)
 {
+    // checks if size is only 6
     if (ID.size() != 6)
     {
         return 0;
     }
+    // checks if first three are letters
     for (int i = 0; i < 3; i++)
     {
+        // uses ascii to compare numbers
+        // if its between 65 and 90
+        // or 97 and 122 for lowercase
+        // it returns true
         if (ID[i] < 65 || (ID[i] > 90 && ID[i] < 97) || ID[i] > 122)
         {
             return 0;
         }
     }
+    // checks if last three are numbers
     for (int i = 3; i < 6; i++)
     {
+        // if its between 48 and 57
+        // returns a true
         if (ID[i] < 48 || ID[i] > 57)
         {
             return 0;
@@ -59,80 +86,100 @@ bool memberType::checkID(string ID)
     }
     return 1;
 }
+// checks if book count is not negative
 bool memberType::checkBookCount(int booksCount)
 {
-    if (booksCount > 0)
+    if (booksCount >= 0)
     {
         return 1;
     }
     return 0;
 }
+// checks if amountspent is correct
 bool memberType::checkAmountSpent(int booksCount, double amountSpent)
 {
+    // checks if book count is 0
+    // amount spent has to be greater than 0
     if (booksCount <= 0 && amountSpent > 0)
     {
         return 0;
     }
+    // checks when book count is greater
+    // amountspent has to be greater than 0
     if (booksCount > 0 && amountSpent <= 0)
     {
         return 0;
     }
     return 1;
 }
+// checks if membership is not negative
 bool memberType::checkMembership(int membership)
 {
-    if (membership > 0)
+    if (membership >= 0)
     {
         return 1;
     }
     return 0;
 }
+// checks if discount is not negative
 bool memberType::checkDiscount(int discount)
 {
-    if (discount > 0)
+    if (discount >= 0)
     {
         return 1;
     }
     return 0;
 }
-
+// returns ID
 string memberType::getID() const
 {
     return memberID;
 }
+// returns first name
 string memberType::getFirstName() const
 {
     return firstName;
 }
+// returns last name
 string memberType::getLastName() const
 {
     return lastName;
 }
+// returns bookspurchesed
 int memberType::getBookCount() const
 {
     return booksPurchased;
 }
+// returns amountspent
 double memberType::getPurchaseBill() const
 {
     return amountSpent;
 }
+// returns lengthofmembership
 int memberType::getLenghtOfMembership() const
 {
     return lengthOfMembership;
 }
+// returns booksdiscount
 double memberType::getDiscount() const
 {
     return booksDiscount;
 }
+// returns discount amount
 double memberType::getDiscountAmount() const
 {
+    // it divides it so it can
+    // be multiplied by discount
     return (amountSpent / 100) * booksDiscount;
 }
+// returns getfinalprice
 double memberType::getFinalPrice() const
 {
+    // final price is amount minus
+    // discounted amount
     return amountSpent - getDiscountAmount();
 }
-
+// gives memory back
 memberType::~memberType()
 {
 }
